a2b4f5a125d4f60050ebcf248a5faddd620eee00
Andre_Rodrigues_JR <lariotyler6@gmail.com>
2023-11-27 12:43:52 +0200

    style

--- src/views/PasswordRecovery.vue
+++ src/views/PasswordRecovery.vue
@@ -36,27 +36,220 @@
                 </div>
                 <button type="submit" class="btn btn-block cadastrobtn" :disabled="senha !== Csenha">Cadastrar-se</button>
             </div>
+            <div v-if="successMessage">{{ successMessage }}</div>
+            <div v-if="errorMessage">{{ errorMessage }}</div>
         </div>
     </div>
     <div v-else>
-        <p>Você precisa estar logado para redefinir a senha. Redirecionando para a página de login...</p>
-        <router-link :to="{ name: 'LoginTeste' }">Ir para a página de login</router-link>
+        <div class="container">
+            <p v-if="showMessage">Você precisa estar logado para redefinir a senha. Redirecionando para a página de login...
+            </p>
+            <router-link v-if="showMessage" :to="{ name: 'LoginTeste' }" @click="redirectAfterDelay">Ir para a página de
+                login</router-link>
+        </div>
     </div>
 </template>
   
 <script>
+import axios from 'axios';
 export default {
     name: 'PasswordRecovery',
     data() {
         return {
-            isLoggedIn: localStorage.getItem('isLoggedIn') === 'true'
+            isLoggedIn: localStorage.getItem('isLoggedIn') === 'true',
+            showMessage: true,
+            email: '',
+            senha: '',
+            Csenha: '',
+            successMessage: '',
+            errorMessage: ''
         };
     },
-    created() {
-        if (!this.isLoggedIn) {
-            this.$router.push({ name: 'LoginTeste' });
+    mounted() {
+        setTimeout(() => {
+            this.showMessage = false;
+            this.redirectAfterDelay();
+        }, 5000);
+    },
+    methods: {
+        redirectAfterDelay() {
+            setTimeout(() => {
+                this.$router.push({ name: 'LoginTeste' });
+            }, 5000000);
+        },
+        async editPassword() {
+      try {
+        const authToken = sessionStorage.getItem('auth-token');
+
+        const response = await axios.put('https://backend.unitec.ac.mz/editarSenha', {
+          email: this.email,
+          senha: this.senha 
+        }, {
+          headers: {
+            Authorization: `Bearer ${authToken}` // Adicionando o token ao header da requisição
+          }
+        });
+
+        if (response.data.success) {
+          this.successMessage = 'Senha redefinida com sucesso!';
+          this.errorMessage = '';
+        } else {
+          this.errorMessage = response.data.message || 'Erro ao redefinir a senha.';
+          this.successMessage = '';
         }
+      } catch (error) {
+        this.errorMessage = error.message || 'Erro ao redefinir a senha.';
+        this.successMessage = '';
+      }
     }
-}
+    },
+};
 </script>
-  
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #d6d6d6;
+}
+
+.container {
+  position: relative;
+  width: 450px;
+  padding: 40px 40px 40px;
+  background-color: #0017b0;
+  border-radius: 10px;
+  text-align: center;
+  /* box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.05),
+    5px 5px 15px rgba(0, 0, 0, 0.5); */
+}
+
+.container h2 {
+  color: #ffffff;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+
+.red-border {
+  border: 3px solid rgb(255, 0, 0);
+}
+
+.green-border {
+  border: 3px solid green;
+}
+
+.container form {
+  text-align: left;
+  margin-top: 40px;
+}
+
+.container form .form-group {
+  margin-top: 20px;
+}
+
+.container form .form-group label {
+  display: block;
+  color: #fff;
+  margin: 5px;
+  font-size: 18px;
+
+}
+
+.container form .form-group input:active {
+  border: none;
+}
+
+.container form .form-group input {
+  width: 100%;
+  height: 50px;
+  background: #ffffff;
+  /* border: none; */
+  outline: none;
+  border-radius: 10px;
+  padding: 5px 15px;
+  font-size: 18px;
+  transition: all 0.3s ease-in-out;
+  color: #03a9f4;
+  /* box-shadow: inset -5px -5px 10px rgba(243, 220, 220, 0.05),
+    inset 5px 5px 15px rgba(0, 0, 0, 0.5); */
+}
+
+.container form button {
+  width: 100%;
+  height: 50px;
+  background: #02abfa;
+  border-radius: 8px;
+  padding: 5px 15px;
+  font-size: 18px;
+  transition: all 0.3s ease-in-out;
+  color: #fff;
+  margin-top: 40px;
+}
+
+.container form button:hover {
+  border: 2px solid #02abfa;
+  color: #fff;
+}
+
+.container .row .cadastrobtn {
+  width: 100%;
+  height: 50px;
+  background: transparent;
+  border: 2px solid #02abfa;
+  outline: none;
+  border-radius: 8px;
+  padding: 5px 15px;
+  font-size: 18px;
+  transition: all 0.3s ease-in-out;
+  color: #fff;
+  margin-top: 20px;
+}
+
+.container form .btn[type="submit"] {
+  cursor: pointer;
+  /* box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.05),
+    5px 5px 15px rgba(0, 0, 0, 0.5); */
+
+}
+
+.container form .btn [type="submit"]:active {
+  cursor: pointer;
+  color: black;
+  /* box-shadow: inset -5px -5px 10px rgba(255, 255, 255, 0.05),
+    inset 5px 5px 15px rgba(0, 0, 0, 0.5); */
+
+}
+
+.forget {
+  margin-top: 30px;
+  color: #fff;
+}
+
+.line-container {
+  display: flex;
+  align-items: center;
+}
+
+hr {
+  flex: 1;
+  margin: 0 5%;
+  height: 4px;
+  background-color: #fff;
+}
+
+.or-text {
+  flex: 0;
+  width: 20%;
+  text-align: center;
+  color: #fff;
+}
+</style>
